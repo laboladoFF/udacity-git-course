cb7973746ff87eb817cb0941bc097fb31af31e75
laboladoFF <labolado@163.com>
2018-08-07 12:55:49 +0800

    Initial commit

---
+++ js/new.js
@@ -0,0 +1,43 @@
+var deck = document.querySelector('.deck');
+var cards = document.getElementsByClassName('card');
+var cardsOpened = [];
+var matchedCards = [];
+
+deck.addEventListener('click', function(event){
+  var target = event.target || event.srcElement;
+  console.log('o');
+  if(target.className.toLowerCase() == 'card'){
+  //console.log(target.children[0].className);
+  openTheCard(target) ;
+  }
+});
+//打开卡片
+function openTheCard(e){
+    if(e.className = "card open show"){//一个等号。两个等号无效
+      addCardToOpenCards(e)
+    }else{
+      e.classList.add('open','show');
+    }
+}
+//建立打开卡片组
+function addCardToOpenCards(e){
+  cardsOpened.push(e.children[0].className);
+  //console.log(cardsOpened);
+  if(cardsOpened.length > 2){
+    console.log('>2');
+    //console.log(cardsOpened[0]);
+  }else{
+    console.log('<2');
+  }
+}
+//检查图案是否一致
+//对children[0],children[1]classname
+//对比成功元素添加match，并添加到marchedCards数组中
+//对比失败元素去掉样式open、show
+function checkPattern(e){
+  if（cardsOpened[0] == cardsOpened[1]）{
+    console.log('match');
+  }else{
+    console.log('nomatch');
+  }
+}
